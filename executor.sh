a17c65897ca1dc304737afaefb4c5dac2673ce63
Julian <jarangol@eafit.edu.co>
2017-10-09 23:14:25 -0500

    changing tests

--- executor.sh
+++ executor.sh
@@ -1,1 +1,13 @@
-#!/usr/bin/env bash
+#!/usr/bin/env
+
+if ! [ -x /usr/bin/nproc ]; then
+    echo "nproc is not installed. Please install it."
+    exit 1
+fi
+CORES=$(nproc)
+
+TEST=1
+#mpiexec -np ${CORES} python ./hello_world${EXAMPLE}.py
+# mpiexec -np 4 python ./test${TEST}.py
+mpiexec -np 4 python ./serial.py
+# mpiexec -np 4 python ./paralelo.py
--- test2.py
+++ test2.py
@@ -1,38 +1,11 @@
-from mpi4py import MPI
-import glob
-import re
+infile = open('Abraham Lincoln___Lincoln Letters.txt', 'r')
+# Mostramos por pantalla lo que leemos desde el fichero
 
-docs = glob.glob("./*.txt")
+items = set()
 
+for line in infile:
+    for word in line.split(' '):
+        items.add(word.rstrip())
+infile.close()
 
-
-comm = MPI.COMM_WORLD
-rank = comm.rank
-size = comm.size
-name = MPI.Get_processor_name()
-
-
-
-def create_set(infile):
-    items = set()
-    for line in infile:
-        for word in line.split(' '):
-            res = re.sub('[^A-Za-z0-9]+', '', word)
-        items.add(res.lower())
-    infile.close()
-    return items
-
-def jaccard(set1,set2):
-    intersection = len(set.intersection(*[set(set1), set(set2)]))
-    union = len(set.union(*[set(set1), set(set2)]))
-    result = (intersection / float(union))
-    return result
-
-
-for j in range(rank,len(docs)):
-    print docs[rank]," comparado con ",docs[j], "rank ",rank
-    set1 = create_set(docs[rank])
-    set2 = create_set(docs[j])
-    jaccard(set1,set2)
-
-
+print(items)
