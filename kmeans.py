4b461c5deb213787ee45fedb91bdfb290de50e25
Julian <jarangol@eafit.edu.co>
2017-10-14 09:34:56 -0500

    kmeans serial parelelizable, falta probar con los docs

--- kmeans.py
+++ kmeans.py
@@ -1,3 +1,4 @@
+#coding=utf-8
 '''Implementation and of K Means Clustering'''
 import numpy as np
 
@@ -11,41 +12,51 @@ def kMeans2(X, K, maxIters = 10):
         centroids = [X[C == k].mean(axis = 0) for k in range(K)]
     return np.array(centroids)
 
+
+# el de nosotros
 def kMeans(X, K, maxIters = 10):
     # generamos k centroides con valores aleatorios
     centroides = np.random.rand(K,len(X))
     for i in range(maxIters):
-        # Cluster Assignment step
-        C = asignar()
-        # Move centroids step
-        centroids = [X[C == k].mean(axis = 0) for k in range(K)]
-    return np.array(centroids)
+        # assinacion de centroides
+        C = asignar(X,centroides)
+        # calculamos el promedio para cada centroide
+        centroides = mover(centroides,K,C)
+    return np.array(centroides)
 
-def asignar():
+def asignar(X,centroids):
     C2 = []
     for xi in X:
-        print "xi",xi
+        # print "xi",xi
         dists = []
         for ci in centroids:
-            print "ci",ci
+            # print "ci",ci
             dist = np.linalg.norm(np.array(xi)-np.array(ci))
             dists.append(dist)
-            print "dist ",dist
+            # print "dist ",dist
         menor = np.argmin(dists)
         C2.append(menor)
-        print "menor: ",dists[np.argmin(dists)]," en ",menor
-    print C2
+        # print "menor: ",dists[np.argmin(dists)]," en ",menor
+    print "asiganacion quedó ",C2
     return C2
 
-def mover():
-    centroids = []
+def mover(centroids,K,C):
     for k in range(K):
         ks = []
-        for c in range(len(C2)):
-            if C2[c] == k:
+        for c in range(len(C)):
+            if C[c] == k:
                 ks.append(X[c])
-        print "k ",k," ks ",ks
+        # print "k ",k," ks ",ks
         if len(ks)>0:
             mean =  np.mean(ks,axis=0)
-        print "centroide ",k," movido de ",centroids[k]," a ",mean
-        centroids[k]=mean
+            # print "centroide ",k," movido de ",centroids[k]," a ",mean
+        # else:
+            # print "centroide sigue en la posicion ",centroids[k]
+    print "centroides quedaron ",centroids
+    return centroids
+
+K = 5
+X = [[ 0.33333333, 0.375, 0.45833333, 0.0, 0.25],[1.0, 0.5, 1.0, 1.0, 0.0 ],
+     [2.0, 0.2, 0.0, 1.0, 0.3 ],[0.01, 1.0, 0.2, 1.0, 0.0 ],[0.7, 0.6, 0.5, 1.0, 0.0 ]]
+print "K: ",K,"X: ",X
+print "result: ",kMeans(X,K)
